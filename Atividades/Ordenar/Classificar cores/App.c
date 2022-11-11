1bc1b70b6fd8102c58884e0957373f2be98fb143
Gustavo Souza <gustavosouza0@yahoo.com>
2022-11-11 20:41:45 -0300

    Finalizado

---
+++ Atividades/Ordenar/Classificar cores/App.c
@@ -0,0 +1,39 @@
+/*
+    !- Gustavo Dias Souza -! 
+     
+    #Atividade de LeetCode 
+
+- Dada uma matriz numscom nobjetos coloridos em vermelho, 
+  branco ou azul, classifique-os no local para que os objetos da mesma cor sejam adjacentes, 
+  com as cores na ordem vermelho, branco e azul.
+- Usaremos os inteiros 0, 1, e 2para representar a cor vermelha, branca e azul, respectivamente.
+- Você deve resolver esse problema sem usar a função de classificação da biblioteca.
+
+*/
+#include <stdio.h>
+
+#include <string.h>
+
+
+int * ordenador(int * nums,int numsSize ) {
+    int i, c[3];
+    
+    for (i = c[0] = c[1] = c[2] = 0; i < numsSize; i++)
+        c[nums[i]]++;
+    
+    for (i = 0; i < c[0]; i++)
+        nums[i] = 0;
+    while (i < c[0] + c[1])
+        nums[i++] = 1;
+    while (i < numsSize)
+        nums[i++] = 2;
+}
+
+
+
+int main() {
+  int Pointer[] = {0,2,1,0,2,2,1,1,1,0};
+  int Tamanho = (int) sizeof(Pointer) / sizeof(Pointer[0]);
+
+  ordenador(Pointer, Tamanho);
+}
